453f7d53032f5ec67c43ed13b6902c039120f242
officeyutong <i@ytbolg.net>
2018-11-22 22:28:32 +0800

    bug fix

--- python_runner.py
+++ python_runner.py
@@ -77,7 +77,7 @@ def run_python_in_docker(callback, code):
         file.write("{}".format(code))
     print_log("Container created.")
     container = client.containers.create("python", "python /temp/{}".format(
-        file_name), tty=True, detach=False,  volumes={tmp_dir: {"bind": "/temp", "mode": "ro"}}, mem_limit="10m", memswap_limit="20m", oom_kill_disable=True, nano_cpus=0.1)
+        file_name), tty=True, detach=False,  volumes={tmp_dir: {"bind": "/temp", "mode": "ro"}}, mem_limit="10m", memswap_limit="20m", oom_kill_disable=True, nano_cpus=int(0.1*1/1e-9))
     # container = client.containers.create("python", "uname -a".format(
     #     file_name), tty=True, detach=False)
     thd = Thread(target=execute_daemon_calc, args=(
