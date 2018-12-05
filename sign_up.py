da2e8cf13e08d5ddd4143d9cff49c2cdd74bafa4
ZhehaoMi <1125263974@qq.com>
2018-12-06 00:03:00 +0800

    重构代码

--- sign_up.py
+++ sign_up.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-from global_vars import config
-
-Path=config.ATTENDANCE_DATA+"group_data.json"
-
-import json,time
-
-Data = {}
-
 def get_reply(context):
-
-    import os
-    # json文件不存在，建立文件
-    if not os.access(Path, os.F_OK):
-        os.makedirs(JsonFilePath)
-    # 读取json文件
-    FILE = open(JsonFilePath,"r")
-    Data=json.load(load_f)
-    FILE.close()
+    from global_vars import config
+    Data = {}
+    file_path=config.ATTENDANCE_DATA+"group_data.json"
+    import os,json,random
     
-    group_id = context['group_id']
+    # 文件不存在，建立文件夹
+    if not os.path.exists(config.ATTENDANCE_DATA):
+        os.makedirs(config.ATTENDANCE_DATA)
+
+    # json不存在，建立空文件
+    if not os.access(file_path, os.F_OK):
+        with open(file_path,'w') as f:
+            json.dump(Data,f)
+
+    # 读取json文件
+    with open(file_path,"r") as f:
+        Data=json.load(f)
+    
+    group_id = str(context['group_id'])
     sender = context['sender']
-    user_id= sender['user_id']
-    nikename = sender['nickname']
+    user_id= str(sender['user_id'])
+    nickname = sender['nickname']
 
     # 群不在字典中
     if group_id not in Data.keys():
@@ -37,7 +37,7 @@ def get_reply(context):
             "date" : "0001-01-01"
         }
 
-    from datatime import datetime,date
+    from datetime import datetime,date
     today = datetime.strptime(str(date.today()),'%Y-%m-%d')
     last_day = datetime.strptime(Data[group_id][user_id]['date'],'%Y-%m-%d')
     
@@ -45,23 +45,21 @@ def get_reply(context):
     #比较上次签到时间，今日已经签到
     if last_day >= today : 
         return "%s今天已经签过到了！\n当前积分：%d\n本月签到次数：%d\n累计群签到次数：%d" % (
-            nickname,Date[group_id][user_id]['rating'],Data[group_id][user_id]['times_month'],Data[group_id][user_id]['times_all'])
+            nickname,Data[group_id][user_id]['rating'],Data[group_id][user_id]['times_month'],Data[group_id][user_id]['times_all'])
 
     # 清零上个月
-    if lats_day.month!=today.month or last_day.year!=today.year:
+    if last_day.month!=today.month or last_day.year!=today.year:
         Data[group_id][user_id]['times_month'] = 0
         
     #签到
-    delta=rand(1,30-datetime.now().hour)
-    Date[group_id][user_id]['rating'] += dalta
+    delta=random.randint(10,50-datetime.now().hour)
+    Data[group_id][user_id]['rating'] += delta
     Data[group_id][user_id]['times_month'] += 1
     Data[group_id][user_id]['times_all'] += 1
-    Data[group_id][user_id]['date'] = "%d-%d-%d" % ()
+    Data[group_id][user_id]['date'] = str(date.today())
 
-    FILE = open(JsonFilePath,"w")
-    json.dump(Data,FILE)
-    FILE.close()
+    with open(file_path,"w") as f:
+        json.dump(Data,f)
     
     return "给%s签到成功了！\n积分增加：%d\n当前积分：%d\n本月签到次数：%d\n累计群签到次数：%d" % (
-        nickname,delta,Date[group_id][user_id]['rating'],Data[group_id][user_id]['times_month'],Data[group_id][user_id]['times_all'])
-    
+        nickname,delta,Data[group_id][user_id]['rating'],Data[group_id][user_id]['times_month'],Data[group_id][user_id]['times_all'])
